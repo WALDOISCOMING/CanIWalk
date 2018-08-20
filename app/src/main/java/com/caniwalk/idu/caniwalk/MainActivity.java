72f98241d290100468ed62949be16b045686758a
kil <forallabouttest@gmail.com>
2018-08-21 00:38:23 +0900

    [Daum API]이전의 잘못된 다음 target package를 현재의 패키지로 변경

--- app/src/main/java/com/caniwalk/idu/caniwalk/MainActivity.java
+++ app/src/main/java/com/caniwalk/idu/caniwalk/MainActivity.java
@@ -1,11 +1,26 @@
 package com.caniwalk.idu.caniwalk;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import net.daum.mf.map.api.MapView;
 
+import java.security.MessageDigest;
+/**
+ Create Date: 2018-08-20
+ Created by: kil kyungwan
+ 기초 다음 api 연동 맵 액티비티 create
+ ------------------------
+ Edit Date: 2018-08-21
+ Edited by: Kil kyungwan
+ 이전의 잘못된 다음 target package를 현재의 패키지로 변경.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         mapView();
     }
-
 
     private void mapView() {
         MapView mapView = new MapView(this);
